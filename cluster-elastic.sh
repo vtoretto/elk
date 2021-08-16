097401523b8561a9b03e7d3d5387de8959d632eb
vtoretto <80073944+vtoretto@users.noreply.github.com>
2021-08-16 10:25:53 -0300

    Update cluster-elastic.sh

--- cluster-elastic.sh
+++ cluster-elastic.sh
@@ -6,6 +6,10 @@ export ES_HOME=/opt/elk-6.4.0/elasticsearch
 export ES_PATH_CONF="$ES_HOME"/../nodes-config
 export PIDS="$ES_HOME"/../pids
 
+msg() {
+    echo "$(date +%F-%T:) $*"
+}
+
 case $1 in
 
   "start")
@@ -30,4 +34,10 @@ case $1 in
         echo "node $l is not running"
       fi
     done < nodes
+    ;;
+    
+  *)
+    msg "Invalid option."
+    echo "Try: $0 start|stop"
+
 esac
