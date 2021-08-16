13b2b06262e2f897e45cbc09cafa6ae2a3125bee
vtoretto <80073944+vtoretto@users.noreply.github.com>
2021-08-16 10:19:17 -0300

    Create cluster-elastic.sh

---
+++ cluster-elastic.sh
@@ -0,0 +1,33 @@
+#! /bin/bash
+
+# ES_HOME was defined in ~/.bashrc vitor's user,
+# means that variable is loaded while logging.
+export ES_HOME=/opt/elk-6.4.0/elasticsearch
+export ES_PATH_CONF="$ES_HOME"/../nodes-config
+export PIDS="$ES_HOME"/../pids
+
+case $1 in
+
+  "start")
+    while read l; do
+      if [ -f "$PIDS"/${l}.pid ]; then
+        echo "node \"${l}\" is already running..." 
+      else
+        nohup "$ES_HOME"/bin/elasticsearch \
+        -Enode.name="$l" \
+        -Epath.logs=/opt/elk-6.4.0/nodes-log/"$l" \
+        -Epath.data=/opt/elk-6.4.0/nodes-data/"$l" -q \
+        -p "$PIDS"/${l}.pid &
+      fi
+    done < nodes
+    ;;
+
+  "stop")
+    while read l; do
+      if [ -f "$PIDS"/${l}.pid ]; then
+        pkill -F "$PIDS"/${l}.pid
+      else
+        echo "node $l is not running"
+      fi
+    done < nodes
+esac
